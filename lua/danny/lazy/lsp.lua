df766798267f70af96c9efdb60ea42f369858e7f
dannyp99 <dannypires99@gmail.com>
2024-09-05 23:31:06 -0400

    Update LSP

--- lua/danny/lazy/lsp.lua
+++ lua/danny/lazy/lsp.lua
@@ -1,87 +1,92 @@
 return {
 
-    "VonHeikemen/lsp-zero.nvim",
+    -- LSP Support
+    "neovim/nvim-lspconfig",
     dependencies = {
-        -- LSP Support
-        {"williamboman/mason.nvim"},
-        {"williamboman/mason-lspconfig.nvim"},
-        {"neovim/nvim-lspconfig"},
-
-        -- Autocompletion
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
         {"hrsh7th/nvim-cmp"},
-        {"hrsh7th/cmp-buffer"},
-        {"hrsh7th/cmp-path"},
-        {"saadparwaiz1/cmp_luasnip"},
         {"hrsh7th/cmp-nvim-lsp"},
-        {"hrsh7th/cmp-nvim-lua"},
+        {"hrsh7th/cmp-buffer"},
+        {"saadparwaiz1/cmp_luasnip"},
 
         -- Snippets
         {"L3MON4D3/LuaSnip"},
-        {"rafamadriz/friendly-snippets"},
     },
     config = function()
-        local lsp = require('lsp-zero')
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+            callback = function(event)
+                local opts = {buffer = event.buf}
 
-        lsp.preset("recommended")
+                vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+            end,
+        })
+
+        local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
-            ensure_installed = { 'rust_analyzer', 'jdtls' },
+            ensure_installed = {'jdtls', 'rust_analyzer'},
             handlers = {
-                lsp.default_setup,
+                function(server_name)
+                    require('lspconfig')[server_name].setup({
+                        capabilities = lsp_capabilities,
+                    })
+                end,
                 lua_ls = function()
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    require('lspconfig').lua_ls.setup(lua_opts)
+                    require('lspconfig').lua_ls.setup({
+                        capabilities = lsp_capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT'
+                                },
+                                diagnostics = {
+                                    globals = {'vim'},
+                                },
+                                workspace = {
+                                    library = {
+                                        vim.env.VIMRUNTIME,
+                                    }
+                                }
+                            }
+                        }
+                    })
                 end,
             }
         })
 
         local cmp = require('cmp')
         local cmp_select = {behavior = cmp.SelectBehavior.Select}
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        })
 
         cmp.setup({
-            mapping = cmp_mappings
-        })
-
-        cmp_mappings['<Tab>'] = nil
-        cmp_mappings['<S-Tab>'] = nil
-
-
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-            sign_icons = {
-                error = 'E',
-                warn = 'W',
-                hint = 'H',
-                info = 'I'
-            }
-        })
-
-        lsp.on_attach(function(client, bufnr)
-            local opts = {buffer = bufnr, remap = false}
-
-            vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-            vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-            vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-            vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-            vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        end)
-
-        lsp.setup()
-
-        vim.diagnostic.config({
-            virtual_text = true
+            sources = cmp.config.sources({
+                {name = 'nvim_lsp'},  
+                {name = 'luasnip'},  
+            }, {
+                {name = 'buffer'},
+            }),
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({select = true}),
+                ['<C-Space>'] = cmp.mapping.complete(),
+            }),
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
         })
     end
 }
